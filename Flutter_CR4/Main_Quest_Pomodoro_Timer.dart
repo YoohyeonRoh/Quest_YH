69d0d14d75ba97e5e4ee41202a847a492636c69b
YoohyeonRoh <132029517+YoohyeonRoh@users.noreply.github.com>
2023-08-10 15:50:39 +0900

    Add files via upload

---
+++ Flutter_CR4/Main_Quest_Pomodoro_Timer.dart
@@ -0,0 +1,50 @@
+/뽀모도로 타이머 메인퀘스트 dartpad 링크입니다./
+
+https://dartpad.dev/?id=d5910c0742e713d27a176ba8eade8002
+
+
+import 'dart:async';
+
+void main() {
+  final pomodoroTimer = PomodoroTimer();
+  pomodoroTimer.start();
+}
+
+class PomodoroTimer {
+  int round = 0;
+
+  Future<void> start() async {
+    while (round < 4) {
+      round++;
+      if (round < 4) {
+        print('$round라운드 작업(work) 25분 시작합니다.');
+        await startCountdown(Duration(minutes: 25));
+        print('$round라운드 휴식(rest) 15분 시작합니다.');
+        await startCountdown(Duration(minutes: 15));
+      } else {
+        print('$round라운드 작업(work) 25분 시작합니다.');
+        await startCountdown(Duration(minutes: 25));
+        print('$round라운드 휴식(rest) 15분 시작합니다.');
+        await startCountdown(Duration(minutes: 15));
+      }
+    }
+    print('Pomodoro 타이머가 종료되었습니다!');
+  }
+
+  Future<void> startCountdown(Duration duration) async {
+    int remainingSeconds = duration.inSeconds;
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (remainingSeconds <= 0) {
+        timer.cancel();
+      } else {
+        String minutes = (remainingSeconds ~/ 60).toString().padLeft(2, '0');
+        String seconds = (remainingSeconds % 60).toString().padLeft(2, '0');
+        print('남은 시간: $minutes:$seconds');
+        remainingSeconds--;
+      }
+    });
+
+    await Future.delayed(duration);
+  }
+}
